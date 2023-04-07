4f1564b33a09097841c1636168ca496cf1fd9318
amanoizumi <a0918233246@gmail.com>
2023-04-07 17:06:41 +0800

    update: 改註解

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 // 熱更新的套件，
 // 但不知道為何，陣列中使用 './pages/**/*.html' 卻無法熱更新 html 檔案
-// 所以將頁面換成了 ejs 檔案
+// 所以將頁面的附檔名換成了 .ejs
 import liveReload from 'vite-plugin-live-reload';
 
 
